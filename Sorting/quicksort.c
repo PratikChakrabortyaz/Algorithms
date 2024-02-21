6f6c066d752a151ae0056d2a2256e4699b868e9a
PratikChakrabortyaz <117933667+PratikChakrabortyaz@users.noreply.github.com>
2024-02-21 21:26:24 +0530

    Update quicksort.c

--- Sorting/quicksort.c
+++ Sorting/quicksort.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
 
-// Function to swap two elements
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+void swap(int *a,int *b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
 }
+int partition(int arr[],int l,int r)
+{
 
-/* This function takes the last element as pivot,
-   places the pivot element at its correct position in sorted
-   array, and places all smaller (smaller than pivot)
-   to the left of the pivot and all greater elements to
-   the right of the pivot */
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1);     // Index of smaller element
-
-    for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
+    int j=l-1;
+    int pivot=arr[r];
+    for(int i=l;i<r;i++)
+    {
+        if(arr[i]<pivot)
+        {
+            j++;
+            swap(&arr[i],&arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+    swap(&arr[j+1],&arr[r]);
+    return j+1;
 }
-
-/* The main function that implements QuickSort
-   arr[] --> Array to be sorted,
-   low --> Starting index,
-   high --> Ending index */
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        /* pi is partitioning index, arr[pi] is now
-           at right place */
-        int pi = partition(arr, low, high);
-
-        // Separately sort elements before partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+void quicksort(int arr[],int l,int r)
+{
+    if(r>l)
+    {
+        int pi=partition(arr,l,r);
+        quicksort(arr,l,pi-1);
+        quicksort(arr,pi+1,r);
     }
 }
 
@@ -54,13 +43,12 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    quickSort(arr, 0, n - 1);
+    quicksort(arr, 0, n - 1);
 
-    printf("Sorted array: ");
+    printf("Sorted array using quick sort: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
 
     return 0;
 }
