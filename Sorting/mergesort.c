3e8573f8a88d2e1334b180c4cd664f2308b7bf57
PratikChakrabortyaz <117933667+PratikChakrabortyaz@users.noreply.github.com>
2024-02-21 21:02:23 +0530

    Update mergesort.c

--- Sorting/mergesort.c
+++ Sorting/mergesort.c
@@ -1,87 +1,74 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to merge two subarrays of arr[].
-// First subarray is arr[l..m].
-// Second subarray is arr[m+1..r].
-void merge(int arr[], int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+void merge(int arr[],int l,int m,int r)
+{
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1];int R[n2];
+    for(i=0;i<n1;i++)
+    {
+        L[i]=arr[l+i];
+    }
+    for(j=0;j<n2;j++)
+    {
+        R[j]=arr[m+1+j];
+    }
+    i=0;j=0;k=l;
+    while(i<n1&&j<n2)
+    {
+        if(L[i]<=R[j])
 
-    // Create temporary arrays
-    int L[n1], R[n2];
-
-    // Copy data to temporary arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    // Merge the temporary arrays back into arr[l..r]
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+        {
+            arr[k]=L[i];
             i++;
-        } else {
-            arr[k] = R[j];
+        }
+        else{
+            arr[k]=R[j];
             j++;
         }
         k++;
     }
 
-    // Copy the remaining elements of L[], if any
-    while (i < n1) {
-        arr[k] = L[i];
+    while(i<n1)
+    {
+        arr[k]=L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of R[], if any
-    while (j < n2) {
-        arr[k] = R[j];
+    while(j<n2)
+    {
+        arr[k]=R[j];
         j++;
         k++;
     }
 }
+void mergesort(int arr[],int l,int r)
+{
+    if(r>l)
+    {
+        int m=l+(r-l)/2;
+        mergesort(arr,l,m);
+        mergesort(arr,m+1,r);
 
-// l is for left index and r is right index of the
-// sub-array of arr to be sorted
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
-        int m = l + (r - l) / 2;
-
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        // Merge the sorted halves
-        merge(arr, l, m, r);
+        merge(arr,l,m,r);
     }
 }
-
-int main() {
+void main()
+{
     int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
+    printf("Enter no. of elements:\n");
+    scanf("%d",&n);
     int arr[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    printf("Enter array elements:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
     }
-
-    mergeSort(arr, 0, n - 1);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    mergesort(arr,0,n-1);
+    printf("Sorted array using merge sort:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",arr[i]);
     }
-    printf("\n");
-
-    return 0;
 }
